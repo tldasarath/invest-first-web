dae615e2deb2b8c77019d93b20f093c2a8961963
tldasarath <tldasarath@gmail.com>
2026-01-16 10:34:54 +0530

    :feat:schedule meeting section added

---
+++ src/Components/HomePage/FormationandMeeting.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import CompanyFormation from './CompanyFormation'
+import ScheduleMeetingSection from './ScheduleMeetingSection'
+
+const FormationandMeeting= () => {
+  return (
+    <>
+    <div>
+      <CompanyFormation/>
+      <ScheduleMeetingSection/>
+    </div>
+    </>
+  )
+}
+
+export default FormationandMeeting
